06863cdd4142b2f3d9aef607c1731ccd5fc9ad04
enkuria <kengeene@gmail.com>
2024-02-05 08:15:26 +0300

    refactor: add store to provider

--- src/index.js
+++ src/index.js
@@ -10,11 +10,16 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { store } from "./store/index";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
